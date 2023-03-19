6b430b77598736c5d691373329f3326017ac24ec
Bot Bahlul <88623122+botbahlul@users.noreply.github.com>
2023-03-20 02:07:53 +0800

    Update MainActivity.java

--- app/src/main/java/com/android/autosrt/MainActivity.java
+++ app/src/main/java/com/android/autosrt/MainActivity.java
@@ -1279,12 +1279,10 @@ public class MainActivity extends AppCompatActivity {
                 if(authority.equals("media")) {
                     idStr = uri.toString().substring(uri.toString().lastIndexOf('/') + 1);
                     System.out.println("media idStr = " + idStr);
-                    return idStr;
                 }
                 else if(authority.startsWith("com.android.providers")) {
                     idStr = DocumentsContract.getDocumentId(uri).split(":")[1];
                     System.out.println("providers idStr = " + idStr);
-                    return idStr;
                 }
 
                 ContentResolver contentResolver = context.getContentResolver();
@@ -1293,7 +1291,7 @@ public class MainActivity extends AppCompatActivity {
                         "_id=?",
                         new String[]{idStr}, null);
                 if (cursor != null && cursor.getCount()>0 && cursor.moveToFirst()) {
-                    //cursor.moveToFirst();
+                    cursor.moveToFirst();
                     try {
                         int idx = cursor.getColumnIndex(MediaStore.Files.FileColumns.DATA);
                         System.out.println("cursor.getString(idx) = " + cursor.getString(idx));
